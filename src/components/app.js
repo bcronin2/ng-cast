ff0228cb453ae5a1b193ea318d137fc432e208c4
Ben Cronin <benc@bens-mbp.lan>
2018-06-30 23:10:03 -0700

    fixup

--- src/components/app.js
+++ src/components/app.js
@@ -1,18 +1,15 @@
-angular.module('video-player')
-  .component('app', {
-    controller: function(youTube) {
-      this.videos = [];
-      this.currentVideo = 0;
-      this.searchService = youTube;
-      this.selectVideo = (index) => {
-        this.currentVideo = index;
-      };
-      this.searchResults = (items) => {
-        this.videos = items;
-      };
-
-      this.searchService.search('dogs', 
-        this.searchResults.bind(this));
-    },  
-    templateUrl: 'src/templates/app.html' 
-  });
+angular.module('video-player').component('app', {
+  controller: function(youTube) {
+    this.videos = [];
+    this.currentVideo = 0;
+    this.searchService = youTube;
+    this.selectVideo = index => {
+      this.currentVideo = index;
+    };
+    this.searchResults = items => {
+      this.videos = items;
+    };
+    this.searchService.search('dogs', this.searchResults.bind(this));
+  },
+  templateUrl: 'src/templates/app.html'
+});
